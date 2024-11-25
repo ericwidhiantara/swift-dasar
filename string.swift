07dd3297d80cfcdbbbc42d0741ef3b674599669e
eric <nukenin.konoha@gmail.com>
2024-11-25 15:01:09 +0700

    feat: init project

---
+++ string.swift
@@ -0,0 +1,17 @@
+//
+//  string.swift
+//  swift-dasar
+//
+//  Created by Eric on 25/11/24.
+//
+
+
+let greeting = "Hello, World!"
+
+print(greeting)
+
+
+var name = "Eric"
+name = String(name) + " Widhi Antara"
+
+print(name)
